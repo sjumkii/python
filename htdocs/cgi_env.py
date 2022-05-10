b0ece212d7a708e0286cd81427d750f3cb32f203
sjumkii <99640300+sjumkii@users.noreply.github.com>
2022-05-10 11:06:59 +0900

    Add files via upload

---
+++ htdocs/cgi_env.py
@@ -0,0 +1,5 @@
+#!python
+print("Content-Type: text/html; charset=utf-8\n")
+print()
+import cgi
+cgi.test()
---
+++ htdocs/syntax/module.py
@@ -0,0 +1,12 @@
+'''
+import math2
+print(math2.average(1,2,3))
+print(math2.plus(1,2))
+print(math2.pi)
+'''
+
+from math2 import average, plus, pi
+
+print(average(1,2,3))
+print(plus(1,2))
+print(pi)
